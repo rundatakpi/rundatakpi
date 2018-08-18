04f80220a7404a97c713592c54e40e0ed247e3f6
543051741@qq.com <543051741@qq.com>
2018-08-18 09:40:23 +0800

    提交预处理相关代码

--- src/com/cn/run/kpi/datamonitor/preprocess/controller/PreProcessMonitorController.java
+++ src/com/cn/run/kpi/datamonitor/preprocess/controller/PreProcessMonitorController.java
@@ -1,8 +1,12 @@
 package com.cn.run.kpi.datamonitor.preprocess.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cn.run.kpi.datamonitor.preprocess.service.PreProcessService;
 
 import net.sf.json.JSONObject;
 
@@ -14,11 +18,18 @@ import net.sf.json.JSONObject;
 @Controller
 @RequestMapping("/preprocess")
 public class PreProcessMonitorController {
+	
+	public static final Logger LOG = Logger.getLogger(PreProcessMonitorController.class);
+	
+	@Autowired
+	private PreProcessService preProcessService;
 
 	@RequestMapping("/init")
 	@ResponseBody
 	public String init() {
 		JSONObject object = new JSONObject();
+		
+		preProcessService.getPreProcessRealTimeData();
 		return object.toString();
 	}
 }
