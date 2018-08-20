4f4b17f00d5aecfc63db005e8e37d6712d1d94a3
Lenovo <Lenovo@LAPTOP-7E2ACEI9>
2018-08-20 16:09:08 +0800

    增加动作类型文件配置

--- src/com/cn/run/kpi/estimate/controller/YCLController.java
+++ src/com/cn/run/kpi/estimate/controller/YCLController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cn.run.kpi.estimate.common.ActionConfig;
 import com.cn.run.kpi.estimate.common.LogConstants;
 import com.cn.run.kpi.estimate.entity.YCLInfo;
 import com.cn.run.kpi.estimate.entity.YCLExample;
@@ -56,6 +58,11 @@ public class YCLController {
 			yclData.setCreateDate(currDate);
 			
 			List<YCLInfo> yclDatas = yclDataService.getList(yclData);
+			for (YCLInfo yclInfo : yclDatas) {
+				String actionTypeDesc = ActionConfig.getValue(yclInfo.getActionType());
+				yclInfo.setActionTypeDesc(actionTypeDesc);
+			}
+			
 			Integer total = yclDataService.getTotal(yclData);
 			
 			resultMap.put("recordsTotal", total);
